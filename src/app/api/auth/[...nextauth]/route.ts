d51bf0c0de5707b9955b5dfb5f5476294f3b5956
NOVA1107 <user@example.com>
2026-03-26 17:12:05 +0800

    fix: add edge runtime config

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,8 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
+export const runtime = 'edge';
+
 const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
